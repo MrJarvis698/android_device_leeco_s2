21d3065f5269de0ab7f88cb311a47a0bc610c70c
CakesTwix <oleg.kiryazov@gmail.com>
2020-11-07 22:27:28 +0200

    s2: kang 1.3 widevine form Daisy

--- device.mk
+++ device.mk
@@ -160,7 +160,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.3-service.widevine
 
 # Fake logprint for fingerprint libs
 PRODUCT_PACKAGES += \
